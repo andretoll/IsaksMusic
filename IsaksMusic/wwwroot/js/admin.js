dfb4457b52f953774890b6b67463435639774e17
andretoll <andretoll@outlook.com>
2018-04-27 11:37:05 +0200

    - Added feature to highlight file name if file has been selected

--- IsaksMusic/wwwroot/js/admin.js
+++ IsaksMusic/wwwroot/js/admin.js
@@ -10,8 +10,19 @@
             startLoadingAnimation();
         }
     });
+
+    /* When choosing file to upload */
+    $('#songUpload').change(function () {
+        if (this.files.length > 0) {
+            $(this).addClass('file-valid');
+        }
+        else {
+            $(this).removeClass('file-valid');
+        }
+    });
 });
 
+/* Function to start loading animation */
 function startLoadingAnimation() {
     $('#songUploadAnim').removeClass('d-none');
 }
