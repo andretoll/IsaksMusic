9dbcd297e7b8c71df19a2360f48c48d68477d389
andretoll <andretoll@outlook.com>
2018-04-30 16:34:42 +0200

    - Added music page - Added wavesurfer.js - Added basic functionality for music player and waveform

---
+++ IsaksMusic/Pages/Music/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IsaksMusic.Data;
+using IsaksMusic.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace IsaksMusic.Pages.Music
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ApplicationDbContext applicationDbContext, IHostingEnvironment hostingEnvironment, IConfiguration configuration)
+        {
+            _applicationDbContext = applicationDbContext;
+            _hostingEnvironment = hostingEnvironment;
+            _configuration = configuration;
+        }
+
+        public IList<Song> Songs { get; set; }
+
+        public async Task OnGet()
+        {
+            Songs = await _applicationDbContext.Songs.ToListAsync();
+        }
+    }
+}
---
+++ IsaksMusic/wwwroot/js/music-player.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+
+    var volumeSlider = $('#volumeSlider');    
+})
+
+/* Wavesurfer options */
+wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'white',
+    progressColor: '#ffc600',
+    barHeight: 2,
+    barWidth: 3,
+    responsive: true,
+    hideScrollbar: true
+});
+
+/* Set initial volume */
+wavesurfer.setVolume(0.5);
+
+/* When song has finished */
+wavesurfer.on('finish', function () {
+    $('#playPauseBtn').children('i').removeClass('fa-pause-circle');
+    $('#playPauseBtn').children('i').addClass('fa-play-circle');
+});
+
+/* When file is ready */
+wavesurfer.on('ready', function () {
+    console.log("Ready");
+    playPause();
+});
+
+/* Volume slider change */
+volumeSlider.oninput = function () {
+
+    var volume = $('#volumeSlider').val();
+    console.log(volume);
+
+    var value = volume / 100;
+    console.log(value);
+
+    wavesurfer.setVolume(value);
+}
+
+var slider = document.querySelector('#slider');
+
+slider.oninput = function () {
+    var zoomLevel = Number(slider.value);
+    wavesurfer.zoom(zoomLevel);
+};
+
+function playSong(file, title) {
+    wavesurfer.load('music/' + file);
+
+    /* Set title */
+    $('#songPlayingTitle').html(title);
+}
+
+function playPause() {
+
+    if (wavesurfer.isPlaying()) {
+        console.log("Pause");
+        wavesurfer.pause();
+        $('#playPauseBtn').children('i').removeClass('fa-pause-circle');
+        $('#playPauseBtn').children('i').addClass('fa-play-circle');
+    }
+    else {
+        console.log("Play");
+        wavesurfer.play();
+        $('#playPauseBtn').children('i').removeClass('fa-play-circle');
+        $('#playPauseBtn').children('i').addClass('fa-pause-circle');
+    }
+}
