935d969e6c5aab4d20f2847b9e149d66120905c5
andretoll <andretoll@outlook.com>
2018-05-15 15:59:25 +0200

    - Added latest news and news feed

---
+++ IsaksMusic/Pages/News/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IsaksMusic.Data;
+using IsaksMusic.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace IsaksMusic.Pages.News
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public IndexModel(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public IList<NewsEntry> NewsEntries { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            NewsEntries = await _applicationDbContext.NewsEntries.OrderByDescending(n => n.PublishDate).Take(1).ToListAsync();
+
+            foreach (var entry in NewsEntries)
+            {
+                if (string.IsNullOrEmpty(entry.ImageUrl))
+                {
+                    entry.ImageUrl = "/images/news-default.jpg";
+                }
+            }
+        }
+    }
+}
